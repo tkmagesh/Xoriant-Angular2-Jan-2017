ff75f77c3d827707c9b6a7d219160d7619cba0fc
Magesh <tkmagesh77@gmail.com>
2017-01-07 14:34:11 +0530

    sort pipe

--- 03-bug-tracker/app/utils/Pipes/Sort.pipe.ts
+++ 03-bug-tracker/app/utils/Pipes/Sort.pipe.ts
@@ -8,10 +8,26 @@ export class SortPipe implements PipeTransform{
     transform(data : Array<any>, attrName : string, isDescending : boolean = false) : Array<any>{
         if (!data.length || !attrName)
             return data;
-        let comparer : IComparer = getComparer(attrName);
+        let comparer : IComparer = this.getComparer(attrName);
         if (isDescending)
-            comparer = getDescending(comparer);
+            comparer = this.getDescending(comparer);
         return data.sort(comparer);
+    }
+
+     getComparer(attrName:string) : IComparer {
+        return function (item1 : any, item2 : any) : number{
+            if (item1[attrName] < item2[attrName])
+                return -1;
+            if (item1[attrName] > item2[attrName])
+                return 1;
+            return 0;
+        }
+    }
+
+    getDescending(comparer :IComparer ) : IComparer{
+        return function(item1 : any, item2 : any){
+            return comparer(item1, item2) * -1;
+        }
     }
 }
 
@@ -19,18 +35,3 @@ interface IComparer {
     (item1 : any, item2 : any ) : number
 }
 
-function getComparer(attrName:string) : IComparer {
-    return function (item1 : any, item2 : any) : number{
-        if (item1[attrName] < item2[attrName])
-            return -1;
-        if (item1[attrName] > item2[attrName])
-            return 1;
-        return 0;
-    }
-}
-
-function getDescending(comparer :IComparer ) : IComparer{
-    return function(item1 : any, item2 : any){
-        return comparer(item1, item2) * -1;
-    }
-}
